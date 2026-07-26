37abd1b37f60297a8b3dfe4cade1e708fe8ec96b
Yordin_CR <yycruzr@unitru.edu.pe>
2026-07-26 13:58:03 -0500

    Clases bases composite creadad -- Inicio de pruebas de menu dinamico

--- SistemaClinicaSanGabriel/src/logica/composite/CategoriaMenu.java
+++ SistemaClinicaSanGabriel/src/logica/composite/CategoriaMenu.java
@@ -4,10 +4,46 @@
  */
 package logica.composite;
 
+import entidades.Rol;
+
+import javax.swing.*;
+import java.util.ArrayList;
+
 /**
  *
  * @author yordi
  */
-public class CategoriaMenu {
-    
+public class CategoriaMenu implements ElementoMenu {
+    private String titulo;
+    private ArrayList<ElementoMenu> elementosHijos;
+
+    public CategoriaMenu(String titulo) {
+        this.titulo = titulo;
+        elementosHijos = new ArrayList<>();
+    }
+
+    public void agregarElementoHijo(ElementoMenu elemento){
+        elementosHijos.add(elemento);
+    }
+
+
+    @Override
+    public JComponent obtenerComponenteSwing(Rol rolUsuario) {
+
+        JMenu menuCategoria = new JMenu(titulo);
+        int elementosVisibles = 0;
+
+        for(ElementoMenu h : elementosHijos){
+            JComponent compHijo = h.obtenerComponenteSwing(rolUsuario);
+
+            if(compHijo != null){
+                menuCategoria.add(compHijo);
+                elementosVisibles++;
+            }
+        }
+
+        if(elementosVisibles == 0) return null;
+
+        return menuCategoria;
+    }
 }
