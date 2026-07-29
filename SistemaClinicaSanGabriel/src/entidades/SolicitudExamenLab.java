685724fe2c362f266e15ef28d0907f06ddb0502a
Challe-1108 <pcchuquipomag@unitru.edu.pe>
2026-07-28 20:36:44 -0500

    0.03

--- SistemaClinicaSanGabriel/src/entidades/SolicitudExamenLab.java
+++ SistemaClinicaSanGabriel/src/entidades/SolicitudExamenLab.java
@@ -1,8 +1,25 @@
 package entidades;
 
 import java.time.LocalDate;
+import java.time.Month;
 
 public class SolicitudExamenLab {
     private int idSolicitud;
+    private int idPaciente;
+    private LocalDate fecha;
+    private String descripcion;
+
+    public SolicitudExamenLab(int idSolicitud, int idPaciente, LocalDate fecha, String descripcion) {
+        this.idSolicitud = idSolicitud;
+        this.idPaciente = idPaciente;
+        this.fecha = fecha;
+        this.descripcion = descripcion;
+    }
     
+    public SolicitudExamenLab(){
+        idSolicitud = 000;
+        idPaciente = 000;
+        fecha = LocalDate.of(2026,07,01);
+        descripcion = "NULL";
+    }
 }
