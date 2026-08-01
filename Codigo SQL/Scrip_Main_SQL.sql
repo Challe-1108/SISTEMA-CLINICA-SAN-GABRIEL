ff4fee3407ff01604a0d9976a49de13593e3adf4
Yordin_CR <yycruzr@unitru.edu.pe>
2026-07-31 22:16:27 -0500

    Modificaciones finales

---
+++ Codigo SQL/Scrip_Main_SQL.sql
@@ -0,0 +1,25 @@
+-- MODULO USUARIOS
+
+CREATE DATABASE sistema_clinica_san_gabriel;
+USE sistema_clinica_san_gabriel;
+
+CREATE TABLE Usuarios (
+    idUsuario INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    rol VARCHAR(30) NOT NULL,
+    estado BOOLEAN DEFAULT TRUE
+);
+
+CREATE TABLE Auditorias (
+    idAuditoria INT AUTO_INCREMENT PRIMARY KEY,
+    idUsuario INT NOT NULL,
+    fecha DATE NOT NULL,
+    hora TIME NOT NULL,
+    modulo VARCHAR(50) NOT NULL,
+    operacion VARCHAR(255) NOT NULL,
+    
+    CONSTRAINT fk_auditoria_usuario 
+        FOREIGN KEY (idUsuario) REFERENCES Usuarios(idUsuario)
+        ON DELETE RESTRICT
+);
--- SistemaClinicaSanGabriel/src/presentacion/FrmPrincipal.java
+++ SistemaClinicaSanGabriel/src/presentacion/FrmPrincipal.java
@@ -182,14 +182,8 @@ public class FrmPrincipal extends javax.swing.JFrame {
                 .agregarCategoria("Pacientes")
                     .agregarItem("Registrar Paciente", e -> {abrirInternalFrame(new IfrmRegistroPaciente());},
                             Rol.ADMINISTRADOR, Rol.RECEPCIONISTA)
-                    .agregarItem("Modificar Paciente", e -> {},
-                            Rol.ADMINISTRADOR, Rol.RECEPCIONISTA)
                     .agregarItem("Buscar Paciente", e -> {abrirInternalFrame(new IfrmBuscarPaciente());},
                             Rol.ADMINISTRADOR, Rol.RECEPCIONISTA, Rol.MEDICO, Rol.ENFERMERA, Rol.CAJERO)
-                    .agregarItem("Seguros Medicos", e -> {},
-                            Rol.CAJERO, Rol.RECEPCIONISTA)
-                    .agregarItem("Historial Clinico", e -> {},
-                            Rol.MEDICO, Rol.DIRECTOR_MEDICO, Rol.ENFERMERA)
 
                 .agregarCategoria("Medicos")
                     .agregarItem("Gestión de Medicos", e -> {abrirInternalFrame(new IfrmGestionMedicos());},
